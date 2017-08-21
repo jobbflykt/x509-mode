358682a59712ab4f8a0469df5472ddcbae17be56
Fredrik Axelsson <f.axelsson@gmail.com>
2017-08-21 19:00:02 +0200

    Add MIT license.

--- x509-mode.el
+++ x509-mode.el
@@ -7,19 +7,28 @@
 
 ;; This file is not part of GNU Emacs.
 
+;; MIT License
+;;
+;; Copyright (c) 2017 Fredrik Axelsson <f.axelsson@gmail.com>
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
 
-;; This file is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License
-;; as published by the Free Software Foundation; either version 2
-;; of the License, or (at your option) any later version.
-
-;; This file is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this file. If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
@@ -27,9 +36,12 @@
 
 ;; Uses OpenSSL for viewing PEM and DER encoded PKI entities.
 
-;; While in a buffer displaying a certificate, use M-x x509-viewcert to create
-;; a new buffer that displays the decoded certificate.  Use M-x x509-viewcrl,
-;; M-X x509-viewasn1, M-x x509-viewkey M-x x509-viewdh in a similar manner.
+;; Usage:
+;; Open a file containing a certficate, either PEM or DER encode. Now
+;; use M-x `x509-viewcert' to create a new buffer that displays the decoded
+;; certificate.
+;; Use M-x `x509-viewcrl', M-X `x509-viewasn1', M-x `x509-viewkey' and M-x
+;; `x509-viewdh' in a similar manner.
 
 ;;; Code:
 

6b4c667979bb14f096f9d44158db0ff676e79ede
Matthew Sojourner Newton <matt@mnewton.com>
2021-08-19 09:16:36 -0700

    Add command x509-viewcsr

--- x509-mode.el
+++ x509-mode.el
@@ -40,8 +40,8 @@
 ;; Open a file containing a certificate, either PEM or DER encode.  Now
 ;; use M-x `x509-viewcert' to create a new buffer that displays the decoded
 ;; certificate.
-;; Use M-x `x509-viewcrl', M-X `x509-viewasn1', M-x `x509-viewkey' and M-x
-;; `x509-viewdh' in a similar manner.
+;; Use M-x `x509-viewcrl', M-X `x509-viewasn1', M-x `x509-viewkey', M-x
+;; `x509-viewdh' and `x509-viewcsr' in a similar manner.
 
 ;;; Code:
 
@@ -355,6 +355,24 @@ For example to enter pass-phrase, add -passin pass:PASSPHRASE."
                 'x509--viewkey-history))
   (x509--process-buffer (split-string-and-unquote args))
   (x509-mode))
+
+;;;###autoload
+(defun x509-viewcsr (&optional args)
+  "Parse current buffer as a certificate signing request file.
+Display result in another buffer.
+
+Append ARGS to the openssl command.
+
+With \\[universal-argument] prefix, you can edit the command arguements."
+  (interactive (x509--read-arguments
+                "x509 args: "
+                (format "req -nameopt multiline,utf8 -text -noout -inform %s"
+                        (x509--buffer-encoding))
+                'x509--viewcsr-history))
+  (x509--process-buffer (split-string-and-unquote args))
+  (x509-mode))
+
+(defvar x509--viewcsr-history nil "History list for x509-viewcsr.")
 
 ;; ----------------------------------------------------------------------------
 ;; asn1-mode
